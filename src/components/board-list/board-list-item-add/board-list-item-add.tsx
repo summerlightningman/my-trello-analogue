18e62587eeaf60a585d928b2a2a897118b3ec6a0
summerlightingman <div8@bk.ru>
2021-09-27 00:36:06 +0300

    added add-board function

--- src/components/board-list/board-list-item-add/board-list-item-add.tsx
+++ src/components/board-list/board-list-item-add/board-list-item-add.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import {FC, FormEventHandler, MouseEventHandler} from 'react';
+
+import {useTypedSelector} from "../../../hooks/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {Board, BoardActionTypes} from "../../../types/board";
 
 import './board-list-item-add.css';
 
-const BoardListItemAdd = () => {
+const BoardListItemAdd: FC = () => {
+    const {isAddingBoard, newBoardName} = useTypedSelector(state => state.board);
+    const dispatch = useDispatch();
+
+    const switchState: MouseEventHandler<HTMLButtonElement> = () =>
+        dispatch({type: BoardActionTypes.SWITCH_ADDING_STATE});
+
+    const handleInput: FormEventHandler<HTMLInputElement> = e =>
+        dispatch({type: BoardActionTypes.SET_NEW_BOARD_NAME, payload: e.currentTarget.value});
+
+    const button = <button onClick={switchState}>Добавить</button>;
+
+    const addBoard: MouseEventHandler<HTMLButtonElement> = () => {
+        const board: Board = {name: newBoardName};
+        dispatch({type: BoardActionTypes.ADD_BOARD, payload: board});
+    };
+
+    const input = <div>
+        <input type="text" value={newBoardName} onInput={handleInput}/>
+        <button onClick={addBoard} disabled={!newBoardName}>Добавить</button>
+        <button onClick={switchState}>Отмена</button>
+    </div>;
+
     return (
         <div className="board-list-item board-list-item-add">
-
+            {isAddingBoard ? input : button}
         </div>
     );
 };

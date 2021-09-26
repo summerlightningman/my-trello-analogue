6de80ce5724eb763633fdd2952360ba708c63ec8
summerlightingman <div8@bk.ru>
2021-09-27 00:20:58 +0300

    added new actions

--- src/store/reducers/boardReducer.ts
+++ src/store/reducers/boardReducer.ts
@@ -1,13 +1,19 @@
 import {BoardAction, BoardActionTypes, BoardState} from "../../types/board";
 
 const initialState: BoardState = {
-    boardList: []
+    boardList: [],
+    newBoardName: '',
+    isAddingBoard: false
 }
 
 export const boardReducer = (state = initialState, action: BoardAction): BoardState => {
     switch (action.type) {
         case BoardActionTypes.ADD_BOARD:
             return {...state, boardList: [...state.boardList, action.payload]}
+        case BoardActionTypes.SWITCH_ADDING_STATE:
+            return {...state, isAddingBoard: !state.isAddingBoard}
+        case BoardActionTypes.SET_NEW_BOARD_NAME:
+            return {...state, newBoardName: action.payload}
         default:
             return initialState
     }
--- src/types/board.ts
+++ src/types/board.ts
@@ -2,7 +2,9 @@ type BoardName = string;
 
 export enum BoardActionTypes {
     SET_BOARD = 'SET_BOARD',
-    ADD_BOARD = 'ADD_BOARD'
+    ADD_BOARD = 'ADD_BOARD',
+    SWITCH_ADDING_STATE = 'SWITCH_ADDING_STATE',
+    SET_NEW_BOARD_NAME = 'SET_NEW_BOARD_NAME'
 }
 
 interface SetBoardAction {
@@ -15,16 +17,27 @@ interface AddBoardAction {
     payload: Board
 }
 
+interface SwitchAddingState {
+    type: BoardActionTypes.SWITCH_ADDING_STATE,
+}
+
+interface SetNewBoardName {
+    type: BoardActionTypes.SET_NEW_BOARD_NAME,
+    payload: BoardName
+}
+
 export interface Board {
     name: BoardName
 }
 
 export interface BoardState {
-    boardList: Board[]
+    boardList: Board[],
+    newBoardName: BoardName,
+    isAddingBoard: boolean
 }
 
 export interface BoardListItemProps {
     name: BoardName
 }
 
-export type BoardAction = SetBoardAction | AddBoardAction
+export type BoardAction = SetBoardAction | AddBoardAction | SwitchAddingState | SetNewBoardName

a60028d597e30b5ed8f4006923126add8d5b289e
summerlightingman <div8@bk.ru>
2021-09-26 21:53:41 +0300

    project was inited

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+import './App.css';
+
+const App: FC = () => {
+    return <>
+        <h1 className="header">My Trello Analogue</h1>
+        <main className="content">
+            <h1>The tramplin is working</h1>
+        </main>
+    </>;
+}
+
+export default App;

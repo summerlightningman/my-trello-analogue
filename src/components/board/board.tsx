c33ea76a57fb97f172d052348e7af130b064da6c
summerlightingman <div8@bk.ru>
2021-09-30 23:01:37 +0300

    released board component

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -3,10 +3,18 @@ import ColumnList from "./column-list/column-list";
 
 import {useHistory} from "react-router-dom";
 
+import {useTypedSelector} from "../../hooks/useTypedSelector";
+import {Board as BoardClass} from "../../types/board";
+
 import './board.css';
 
 const Board: FC = () => {
     const history = useHistory();
+
+    const {boardList} = useTypedSelector(state => state.board);
+    const [, , boardId] = history.location.pathname.split('/');
+    const [board] = boardList.filter((board: BoardClass) => board.id === +boardId);
+
 
     const back: MouseEventHandler = () => {
         history.goBack();
@@ -14,7 +22,10 @@ const Board: FC = () => {
 
     return (
         <div className="board">
-            <button className="buttons-panel__btn" onClick={back}>Назад</button>
+            <div>
+                <h2>Board: {board.name}</h2>
+                <button className="buttons-panel__btn" onClick={back}>Назад</button>
+            </div>
             <ColumnList/>
         </div>
     );

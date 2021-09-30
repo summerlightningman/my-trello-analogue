d9b9b11f6ea629e41f7e442e49acfa9894954438
summerlightingman <div8@bk.ru>
2021-09-30 23:18:30 +0300

    added column-list-item-add

---
+++ src/components/board/column-list/column-list-item-add/column-list-item-add.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+import './column-list-item-add.css';
+
+const ColumnListItemAdd: FC = () => {
+    return (
+        <div>
+            Column list item add
+        </div>
+    );
+};
+
+export default ColumnListItemAdd;

4c19f3c7b71ba469022ae1b11b8760f633347fdb
summerlightingman <div8@bk.ru>
2021-10-13 16:16:19 +0300

    bugfixies with non-exist board

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -6,12 +6,14 @@ import {useHistory} from "react-router-dom";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
 import {Board as BoardClass} from "../../types/board";
 
-import './board.css';
 import {useDispatch} from "react-redux";
 import {BoardActionTypes} from "../../store/types/board";
 
+import './board.css';
+
 const Board: FC = () => {
     const history = useHistory();
+    const back: MouseEventHandler = () => history.goBack();
 
     const {boardList} = useTypedSelector(state => state.board);
     const dispatch = useDispatch();
@@ -20,18 +22,20 @@ const Board: FC = () => {
     const [board] = boardList.filter((board: BoardClass) => board.id === +boardId);
 
     useEffect(() => {
-            dispatch({type: BoardActionTypes.SET_WINDOW_TITLE, payload: `Board: ${board.name}`})
+        if (board)
+            dispatch({type: BoardActionTypes.SET_WINDOW_TITLE, payload: `Board: ${board.name}`});
+        else
+            dispatch({type: BoardActionTypes.SET_WINDOW_TITLE, payload: '404: Board not found!'});
         },
-        [board.name, dispatch]);
+        [board, dispatch]);
 
-    const back: MouseEventHandler = () => history.goBack();
 
     return (
         <div className="board">
             <div>
                 <button className="buttons-panel__btn" onClick={back}>Back</button>
             </div>
-            <ColumnList board={board}/>
+            {board && <ColumnList board={board}/>}
         </div>
     );
 };

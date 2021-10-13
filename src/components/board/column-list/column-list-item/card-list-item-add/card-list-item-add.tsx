89b457d6aea91bde1f425a0d879fe8c8cc2bf215
summerlightingman <div8@bk.ru>
2021-10-13 15:25:07 +0300

    added CardListItemAdd component

---
+++ src/components/board/column-list/column-list-item/card-list-item-add/card-list-item-add.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './card-list-item-add.css';
+
+const CardListItemAdd = () => {
+    return (
+        <li className="card-list-item card-list-item-add">
+
+        </li>
+    );
+};
+
+export default CardListItemAdd;

969d03e219cb01e5761c61e7e171c071177001b6
summerlightingman <div8@bk.ru>
2022-02-01 20:51:07 +0300

    added DndProvider

--- src/components/Card/card-list/card-list.tsx
+++ src/components/Card/card-list/card-list.tsx
@@ -1,12 +1,12 @@
 import {FC, useContext} from 'react';
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 import CardListItem from "../card-list-item/card-list-item";
 import CardListItemAdd from "../card-list-item-add/card-list-item-add";
-
-
 import {useTypedSelector} from "../../../hooks/useTypedSelector";
-import {ColumnContext} from "../../Column/column-list-item/column-list-item";
 
+import {ColumnContext} from "../../Column/column-list-item/column-list-item";
 import './card-list.css';
 
 const CardList: FC = () => {
@@ -18,7 +18,9 @@ const CardList: FC = () => {
     return (
         <ul className="card-list">
             <CardListItemAdd key={-1} cardCount={cardList.length}/>
-            {cardList.map(card => <CardListItem card={card}  key={card.id}/>)}
+            <DndProvider backend={HTML5Backend}>
+                {cardList.map(card => <CardListItem card={card}  key={card.id}/>)}
+            </DndProvider>
         </ul>
     );
 };

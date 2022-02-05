4e9042c626784031d8f8b076aa807b9cacc6875e
summerlightingman <div8@bk.ru>
2022-02-05 17:06:42 +0300

    added styled-component for ColumnList

--- src/components/Column/column-list/column-list.tsx
+++ src/components/Column/column-list/column-list.tsx
@@ -1,21 +1,27 @@
 import React, {FC} from 'react';
+import {useTypedSelector} from "../../../hooks/useTypedSelector";
+import styled from "styled-components";
+
 import ColumnListItemAdd from "../column-list-item-add/column-list-item-add";
 import ColumnListItem from "../column-list-item/column-list-item";
-
-import {useTypedSelector} from "../../../hooks/useTypedSelector";
-
 import {ColumnListProps} from "../../../types/column";
 
-const ColumnList: FC<ColumnListProps> = ({board}) => {
 
+const ColumnListComponent = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: flex-start;
+`;
+
+const ColumnList: FC<ColumnListProps> = ({board}) => {
     const columnList = useTypedSelector(state => state.board.columnList)
         .filter(col => col.boardId === board.id);
 
     return (
-        <ul className="column-list">
+        <ColumnListComponent>
             <ColumnListItemAdd board={board}/>
             {columnList.map(column => <ColumnListItem column={column} key={column.id}/>)}
-        </ul>
+        </ColumnListComponent>
     );
 };
 

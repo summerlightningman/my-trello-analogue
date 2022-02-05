14363f00955d9ce389d0e803730219ea13754f3f
summerlightingman <div8@bk.ru>
2022-02-05 11:47:16 +0300

    Added styled-component for App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,36 @@
 import {FC} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import {useTypedSelector} from "../../hooks/useTypedSelector";
+import styled from "styled-components";
 
 import BoardList from "../Board/board-list/board-list";
 
 
 import Board from "../Board/board/board";
 
-import './App.css';
-import '../../styles/buttons-panel.css';
+
+const Main = styled.main`
+  padding: 0 10%;
+`;
+
+const Header = styled.header`
+  height: 40px;
+  padding: 10px 10%;
+  border-bottom: 1px solid #0002;
+  font-size: 48px;
+`;
 
 const App: FC = () => {
     const {windowTitle} = useTypedSelector(state => state.board);
 
     return <>
-        <h1 className="header">{windowTitle}</h1>
-        <main className="content">
+        <Header>{windowTitle}</Header>
+        <Main>
             <Router>
                 <Route path="/" exact><BoardList/></Route>
                 <Route path="/board/:id"><Board/></Route>
             </Router>
-        </main>
+        </Main>
     </>;
 }
 

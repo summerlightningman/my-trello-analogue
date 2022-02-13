fcda7dc64d5bddeeaec8b153d870b42ca26cb120
summerlightingman <div8@bk.ru>
2022-02-14 00:53:23 +0300

    refactoring in test suite for Card

--- src/test/card.test.ts
+++ src/test/card.test.ts
@@ -1,6 +1,10 @@
-import {Card} from "../types/card";
+import {Card, CardID, CardName} from "../types/card";
+import {ColumnID} from "../types/column";
 
-const card = new Card(113, 123, 'MyCard');
+const columnId = 113;
+const cardId = 123;
+const cardName = 'MyCard'
+const card = new Card(columnId, cardId, cardName);
 
 describe('Card creating is correct', () => {
 
@@ -30,14 +34,15 @@ describe('Card functions works correctly', () => {
 });
 
 describe('Card list changes correctly', () => {
-    // @ts-ignore
-    const cardList = [[0,0,"mycard1"],[0,1,"mycard2"],[1,0,"mycard3"],[1,1,"mycard4"]].map(([a, b, c]) => new Card(a, b, c));
+    const cardDataList: Array<[ColumnID, CardID, CardName]> =
+        [[0, 0, "mycard1"], [0, 1, "mycard2"], [1, 0, "mycard3"], [1, 1, "mycard4"]];
+    const cardList: Array<Card> = cardDataList.map(args => new Card(...args));
 
     test('Card list IDs will increment to 1', () => {
         const newCardList = cardList.map(card => card.setId(card.id + 1));
-        newCardList.forEach((card, idx) => {
+        newCardList.forEach((card, idx) =>
             expect(card.id - cardList[idx].id).toEqual(1)
-        });
+        );
     });
 
     test('Card list column IDs will increment to 1', () => {
@@ -45,5 +50,20 @@ describe('Card list changes correctly', () => {
         newCardList.forEach((card, idx) => {
             expect(card.columnId - cardList[idx].columnId).toEqual(1)
         });
+    });
+
+    test('Card column ID changing works correctly', () => {
+        const randomIdx = Math.round(Math.random() * (cardList.length - 1));
+        const card: Card = cardList[randomIdx];
+        const newColumnId = 567;
+        expect(card.setColumnId(newColumnId).columnId).toEqual(newColumnId);
+    });
+
+    test('Card ID changing works correctly', () => {
+        const randomIdx = Math.round(Math.random() * (cardList.length - 1));
+        const card: Card = cardList[randomIdx];
+        console.log(randomIdx);
+        const newId = 983;
+        expect(card.setId(newId).id).toEqual(newId);
     })
-})
+});
